de050303dbbf3ce9463c9e4aa4457c6cfa8204f0
DracoLich <mebungler@gmail.com>
2018-07-25 19:15:16 +0500

    Registred to uow

--- SMSApp/Startup/Bootstrapper.cs
+++ SMSApp/Startup/Bootstrapper.cs
@@ -32,11 +32,15 @@ namespace SMSApp.Startup
 
 
             builder.RegisterType<FarmOwnerManagerView>().AsSelf();
+            builder.RegisterType<FarmOwnerAddView>().AsSelf();
             builder.RegisterType<FarmManagerView>().AsSelf();
+            builder.RegisterType<FarmAddView>().AsSelf();
             builder.RegisterType<PlantManagerView>().AsSelf();
+            builder.RegisterType<PlantAddView>().AsSelf();
             builder.RegisterType<DiseaseManagerView>().AsSelf();
+            builder.RegisterType<DiseaseAddView>().AsSelf();
             builder.RegisterType<PestManagerView>().AsSelf();
-
+            builder.RegisterType<PestAddView>().AsSelf();
             builder.RegisterType<UserAddView>().AsSelf();
             builder.RegisterType<UserAddViewModel>().AsSelf();
 
